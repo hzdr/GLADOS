60ff8f6fde5ef8923a076e34e04858cae1de5455
Jan Stephan <j.stephan@hzdr.de>
2016-11-25 13:43:56 +0100

    Added LGPLv3 license
    Closes #5.

--- include/ddrf/cuda/bits/host_allocator.h
+++ include/ddrf/cuda/bits/host_allocator.h
@@ -1,3 +1,25 @@
+/*
+ * This file is part of the ddrf library.
+ *
+ * Copyright (C) 2016 Helmholtz-Zentrum Dresden-Rossendorf
+ *
+ * ddrf is free software: You can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ddrf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ddrf. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Date: 14 July 2016
+ * Authors: Jan Stephan <j.stephan@hzdr.de>
+ */
+
 #ifndef DDRF_CUDA_BITS_HOST_ALLOCATOR_H_
 #define DDRF_CUDA_BITS_HOST_ALLOCATOR_H_
 
--- include/ddrf/memory.h
+++ include/ddrf/memory.h
@@ -1,3 +1,25 @@
+/*
+ * This file is part of the ddrf library.
+ *
+ * Copyright (C) 2016 Helmholtz-Zentrum Dresden-Rossendorf
+ *
+ * ddrf is free software: You can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ddrf is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ddrf. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Date: 14 July 2016
+ * Authors: Jan Stephan <j.stephan@hzdr.de>
+ */
+
 #ifndef DDRF_MEMORY_H_
 #define DDRF_MEMORY_H_
 
@@ -5,4 +27,4 @@
 #include <ddrf/bits/memory_location.h>
 #include <ddrf/bits/pool_allocator.h>
 
-#endif /* MEMORY_H_ */
+#endif /* DDRF_MEMORY_H_ */
